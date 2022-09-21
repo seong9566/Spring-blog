a908e5acc868a5ac6ec900a49510d7e5b9f77191
seong0210 <dlgustjd9566@gmail.com>
2022-09-21 16:02:05 +0900

    users회원가입 유효성 검사

--- src/main/resources/static/js/users.js
+++ src/main/resources/static/js/users.js
@@ -40,17 +40,125 @@ $("#btnUpdate").click(() => {
 	update();
 });
 // =======================함수============================
+// username에 한글 체크 
+function koreanCheck() {
+	let username = $("#username").val();
+	let korRule = /^[가-힣]*$/;
+	if (korRule.test(username)) {
+		return true;
+	} else {
+		return false;
+	}
+}
+// 대문자 체크
+function capitalCheck() {
+	let username = $("#username").val();
+	let checkcapital = /[A-Z]/;
+	if (checkcapital.test(username)) {
+		return false;
+	}
+	else {
+		return true;
+	}
+}
+// 이메일 형식 체크 
+function emailCheck() {
+	let email = $("#email").val();
+	let checkText = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-Za-z0-9\-]+/;
+	if (checkText.test(email)) {
+		return true; // 정규표현식이면 true
+	}
+	else {
+		return false;//아니면 false
+	}
+}
+//이메일 공백 체크 
+function spaceEmail() {
+	let checkspace = /\s/g;
+	let email = $("#email").val();
+	if (checkspace.test(email)) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+//이메일 한글 체크 
+function koreanCheckEmail() {
+	let email = $("#email").val();
+	let korRule = /^[가-힣]*$/;
+	if (korRule.test(email)) {
+		return true;
+	} else {
+		return false;
+	}
+}
+//유저이름 공백 체크
+function spaceUsername() {
+	let checkspace = /\s/g;
+	let username = $("#username").val();
+	if (checkspace.test(username)) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+//패스워드 공백 체크
+function spacePassword() {
+	let checkspace = /\s/g;
+	let password = $("#password").val();
+	if (checkspace.test(password)) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+// 패스워드 한글 체크 
+function koreanCheckPassword() {
+	let password = $("#password").val();
+	let korRule = /^[가-힣]*$/;
+	if (korRule.test(password)) {
+		return true;
+	} else {
+		return false;
+	}
+}
 
 // 회원 가입 함수
-function join(){
-		if (isUsernameSameCheck == false) {
+function join() {
+	if (isUsernameSameCheck == false) {
 		alert("유저네임 중복 체크를 진행 해주세요");
 		return;
 	}
+
+	if (koreanCheck() == true || spaceUsername() == true) {
+		alert("username에 한글 또는 공백을 넣지마세요");
+		return;
+	}
+
+	if (capitalCheck() == true) {
+		alert("username에 대문자 하나이상을 입력하세요.");
+		return;
+	}
+	
+	if (spacePassword() == true || koreanCheckPassword()== true) {
+		alert("비밀번호에 공백이나 한글을 넣지마세요 .");
+		return;
+	}
+	
 	if (isPasswordCheck == false) {
 		alert("비밀번호 체크를 진행 해주세요.");
 		return;
 	}
+	
+	if (emailCheck() == false || spaceEmail() == true || koreanCheckEmail()== true) {
+		alert("이메일형식이 올바르지 않습니다.");
+		return;
+	}
+
+
 	let data = {
 		username: $("#username").val(),
 		password: $("#password").val(),
@@ -60,7 +168,7 @@ function join(){
 		type: "POST",
 		dataType: "JSON",
 		data: JSON.stringify(data),
-		headers: { 
+		headers: {
 			"Content-Type": "application/json"
 		}
 	}).done((res) => {
@@ -71,8 +179,8 @@ function join(){
 }
 
 // 패스워드 체크 
-function checkPassword(){
-		let passwordCheck = $("#passwordCheck").val();
+function checkPassword() {
+	let passwordCheck = $("#passwordCheck").val();
 	let password = $("#password").val();
 	if (password == passwordCheck) {
 		alert("패스워드가 일치합니다");
@@ -85,12 +193,12 @@ function checkPassword(){
 }
 
 // 유저 네임 중복 체크 
-function checkUserName(){
-		let username = $("#username").val();
+function checkUserName() {
+	let username = $("#username").val();
 	$.ajax(`/api/users/usernameSameCheck?username=${username}`, {// JS이므로 백틱 사용 가능  
 		type: "GET",
-		dataType: "json", 
-		async: true 
+		dataType: "json",
+		async: true
 	}).done((res) => {
 		if (res.code == 1) {
 			if (res.data == false) {
@@ -104,14 +212,14 @@ function checkUserName(){
 			}
 		}
 
-	}); 
+	});
 }
 
 
 
 //로그인
-function login(){
-		let data = {
+function login() {
+	let data = {
 		username: $("#username").val(),
 		password: $("#password").val(),
 		remember: $("#remember").prop("checked")
@@ -135,7 +243,7 @@ function login(){
 }
 
 // 회원 탈퇴
-function resign(){
+function resign() {
 	let id = $("#id").val();
 
 	$.ajax("/s/api/users/" + id, {
@@ -152,7 +260,7 @@ function resign(){
 }
 
 // 회원 정보 수정
-function update(){
+function update() {
 	let data = {
 		password: $("#password").val(),
 		email: $("#email").val()
@@ -163,7 +271,7 @@ function update(){
 
 	$.ajax("/s/api/users/" + id, {
 		type: "PUT",
-		dataType: "json", 
+		dataType: "json",
 		data: JSON.stringify(data),
 		headers: {
 			"Content-Type": "application/json; charset=utf-8"
@@ -171,10 +279,12 @@ function update(){
 	}).done((res) => {
 		if (res.code == 1) {
 			alert("회원 수정 완료");
-			location.reload(); 
+			location.reload();
 		} else {
 			alert("업데이트에 실패하였습니다");
 		}
 	});
+
+
 }
 
--- src/main/webapp/WEB-INF/views/users/joinForm.jsp
+++ src/main/webapp/WEB-INF/views/users/joinForm.jsp
@@ -9,17 +9,20 @@
 			<button id= "btnUserNameSameCheck"class = "btn btn-warning" type="button">UserName 중복 체크</button>
 		</div>
 		<div class="mb-3">
-			<input id="password"type="password" class="form-control" placeholder="Enter password">
-			<input id="passwordCheck"type="password" class="form-control" placeholder="Onemore password">
+			<input id="password"type="password" class="form-control" placeholder="Enter password"maxlength=20>
+			<input id="passwordCheck"type="password" class="form-control" placeholder="Onemore password" maxlength=20>
 			<button id= "btnPasswordCheck"class = "btn btn-warning" type="button">password 체크</button>
 		</div>
 		<div class="mb-3">
-			<input id="email"type="email" class="form-control" placeholder="Enter email">
+			<input id="email"type="email" class="form-control" placeholder="Enter email" maxlength=20>
 		</div>
 		<button id = "btnJoin"type="button" class="btn btn-primary">회원가입</button>
 	</form>
 </div>
 
+<script>
+
+</script>
 <script src="/js/users.js"></script>
 <%@ include file="../layout/footer.jsp"%>
 
